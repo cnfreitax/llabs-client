48ef61fedbed672ed7f841d937a42437d49216a4
Victor Freitas <victorprogr@gmail.com>
2022-02-11 01:29:33 -0300

    chore: add styled-components and create global styles

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { GlobalStyles } from 'styles/global-styles'
 
 function App() {
-	return <div>ok</div>
+	return (
+		<>
+			<div>ok</div>
+			<GlobalStyles />
+		</>
+	)
 }
 
 export default App
