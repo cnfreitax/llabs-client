661913426e33091a494b2b78c7e8343a7d952da9
Victor Freitas <victorprogr@gmail.com>
2022-02-11 04:15:14 -0300

    chore: add themePorivder on app

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { GlobalStyles } from 'presentation/styles/global-styles'
+import { Main } from 'presentation/layouts/Main'
+import theme from 'presentation/styles/theme'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
 	return (
-		<>
-			<div>ok</div>
+		<ThemeProvider theme={theme}>
+			<Main />
 			<GlobalStyles />
-		</>
+		</ThemeProvider>
 	)
 }
 
