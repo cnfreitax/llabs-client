85d1c6bf06244c7a5cc30655b04e5080610aceab
Victor Freitas <victorprogr@gmail.com>
2022-02-11 04:16:42 -0300

    feat: add global text component

---
+++ src/presentation/shared/components/Text/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import * as S from './styles'
+
+export type TextProps = {
+	color?: 'primary' | 'gray'
+	size?:
+		| 'xlarge'
+		| 'xsmall'
+		| 'small'
+		| 'medium'
+		| 'large'
+		| 'xxxsmall'
+		| 'xxsmall'
+		| 'xxlarge'
+		| 'xxxlarge'
+	fontWeight?: 100 | 200 | 300 | 500 | 600
+	lineHeight?: string | number
+	as?: React.ElementType
+	children: React.ReactNode
+}
+
+export const Text = ({
+	children,
+	color,
+	size,
+	fontWeight,
+	lineHeight,
+	as
+}: TextProps) => {
+	return (
+		<S.Heading
+			color={color}
+			size={size}
+			fontWeight={fontWeight}
+			lineHeight={lineHeight}
+			as={as}
+		>
+			{children}
+		</S.Heading>
+	)
+}
