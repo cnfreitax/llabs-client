1080119e0c0134722e4933ed770106b731602e51
Victor Freitas <victorprogr@gmail.com>
2022-02-12 02:16:11 -0300

    layout: create menu home screen

--- src/presentation/layouts/Main/index.tsx
+++ src/presentation/layouts/Main/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Wrapper } from 'presentation/shared/components/Wrapper'
+import { Wrapper, Text } from 'presentation/shared/components'
+
+import IconHero from 'presentation/shared/assets/icons/ic_heroi.svg'
+
+import * as S from './styles'
+import Button from 'presentation/shared/components/Button'
+import { SvgHero } from 'presentation/shared/assets/icons/svg/SvgHero'
+import { SvgHearth } from 'presentation/shared/assets/icons/svg/SvgHearth'
 
 export const Main = () => {
 	return (
 		<Wrapper>
-			<div>oi</div>
+			<S.Container>
+				<S.Menu>
+					<Text color="lightGray" as="span" fontWeight={400} size="small">
+						Encontrados 20 heróis
+					</Text>
+
+					<S.MenuOptions>
+						<Button variant="text" icon={<SvgHero />} active={true}>
+							Ordenar por nome - A/Z
+						</Button>
+
+						<Button variant="text" icon={<SvgHearth />} active={true}>
+							Somente favoritos
+						</Button>
+					</S.MenuOptions>
+				</S.Menu>
+			</S.Container>
 		</Wrapper>
 	)
 }
---
+++ src/presentation/layouts/Main/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	flex: 1;
+	display: flex;
+	width: 100%;
+	margin-top: 6rem;
+`
+
+export const Menu = styled.div`
+	flex: 1;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	padding: 8px 0;
+	justify-content: space-between;
+`
+
+export const MenuOptions = styled.div`
+	display: flex;
+	align-items: center;
+`
