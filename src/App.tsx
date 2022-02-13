8f6de7bb95df357fb4eb3c489f13f9848c3b8495
Victor Freitas <victorprogr@gmail.com>
2022-02-13 12:04:10 -0300

    add heroProvider on App

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { GlobalStyles } from 'presentation/styles/global-styles'
 import { Main } from 'presentation/layouts/Main'
 import theme from 'presentation/styles/theme'
 import { ThemeProvider } from 'styled-components'
+import { MoviesProvider } from 'presentation/context/heros'
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Main />
+			<MoviesProvider>
+				<Main />
+			</MoviesProvider>
 			<GlobalStyles />
 		</ThemeProvider>
 	)
