15e4fb3ff11a98434f2c0256ad33eb37f6c60908
Victor Freitas <victorprogr@gmail.com>
2022-02-13 19:15:39 -0300

    feat: create rating component

---
+++ src/presentation/shared/components/Rating/index.tsx
@@ -0,0 +1,22 @@
+import { SVGRatingStart } from 'presentation/shared/assets/icons/svg/SVGRating'
+import React from 'react'
+
+import { Text } from '../Text'
+import * as S from './style'
+
+export const Rating = () => {
+	return (
+		<S.Container>
+			<Text as="span" color="gray" size="xxxsmall">
+				Rating:
+			</Text>
+			<S.Stars ratingHero={4}>
+				{[...Array(5)].map((star) => (
+					<div key={star}>
+						<SVGRatingStart />
+					</div>
+				))}
+			</S.Stars>
+		</S.Container>
+	)
+}
