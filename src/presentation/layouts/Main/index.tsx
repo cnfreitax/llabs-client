e711bd3779288e5ecf38ae629313e970598b8a90
Victor Freitas <victorprogr@gmail.com>
2022-02-15 02:07:55 -0300

    feat: add order by asc or dsc list and list only favorites

--- src/presentation/layouts/Main/index.tsx
+++ src/presentation/layouts/Main/index.tsx
@@ -6,6 +6,7 @@ import Button from 'presentation/shared/components/Button'
 import { SvgHero } from 'presentation/shared/assets/icons/svg/SvgHero'
 import { SvgHearth } from 'presentation/shared/assets/icons/svg/SvgHearth'
 import { Hero } from 'domain/protocols/hero'
+import useHeroes from 'presentation/context/heros'
 
 type Props = {
 	heroes: Array<Hero>
@@ -13,6 +14,37 @@ type Props = {
 }
 
 export const MainLayout = ({ heroes, isLoading }: Props) => {
+	const [orderList, setOrderList] = React.useState<'ASC' | 'DESC'>('ASC')
+	const [onlyFavorites, setOnlyFavorites] = React.useState<boolean>(false)
+
+	const { favoritedHeoroes } = useHeroes()
+
+	const formatedList = React.useMemo(() => {
+		if (orderList === 'ASC') {
+			return heroes.sort((a, b) => {
+				if (a.name > b.name) return 1
+				if (a.name < b.name) return -1
+				return 0
+			})
+		} else {
+			return heroes.sort((a, b) => {
+				if (a.name < b.name) return 1
+				if (a.name > b.name) return -1
+				return 0
+			})
+		}
+	}, [orderList, heroes])
+
+	const listOfFavorites = () =>
+		heroes.filter((hero) => favoritedHeoroes.includes(hero.id))
+
+	const handleWithSwitchOrder = () =>
+		orderList === 'ASC' ? setOrderList('DESC') : setOrderList('ASC')
+
+	const handleSelectFavorites = () => {
+		setOnlyFavorites(!onlyFavorites)
+	}
+
 	return (
 		<Wrapper withText>
 			<S.Container>
@@ -23,22 +55,38 @@ export const MainLayout = ({ heroes, isLoading }: Props) => {
 						</Text>
 
 						<S.MenuOptions>
-							<Button variant="text" icon={<SvgHero />} active={true}>
-								Ordenar por nome - A/Z
+							<Button
+								onClick={() => handleWithSwitchOrder()}
+								variant="text"
+								icon={<SvgHero />}
+								active={true}
+							>
+								Ordenar por nome - {orderList === 'ASC' ? 'A/Z' : 'Z/A'}
 							</Button>
 
-							<Button variant="text" icon={<SvgHearth />} active={true}>
+							<Button
+								onClick={() => handleSelectFavorites()}
+								variant="text"
+								icon={<SvgHearth />}
+								active={onlyFavorites}
+							>
 								Somente favoritos
 							</Button>
 						</S.MenuOptions>
 					</S.Menu>
 
 					<ul>
-						{heroes.map((hero) => (
-							<li key={hero.name}>
-								<HeroCard hero={hero} withFavoriteButton size="large" />
-							</li>
-						))}
+						{onlyFavorites
+							? listOfFavorites().map((hero) => (
+									<li key={hero.name}>
+										<HeroCard hero={hero} withFavoriteButton size="large" />
+									</li>
+							  ))
+							: formatedList.map((hero) => (
+									<li key={hero.name}>
+										<HeroCard hero={hero} withFavoriteButton size="large" />
+									</li>
+							  ))}
 					</ul>
 				</S.Main>
 			</S.Container>
