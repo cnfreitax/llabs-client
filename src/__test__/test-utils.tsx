49d9641301a33a97710bc140c0e171df4278cb9e
Victor Freitas <victorprogr@gmail.com>
2022-02-15 16:12:45 -0300

    chore: create test utils

---
+++ src/__test__/test-utils.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react'
+import { render } from '@testing-library/react'
+
+import { ThemeProvider } from 'styled-components'
+import theme from 'presentation/styles/theme'
+
+import { createBrowserHistory } from 'history'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+
+const history = createBrowserHistory()
+
+const customRender = (ui: ReactElement) =>
+	render(
+		<HistoryRouter history={history}>
+			<ThemeProvider theme={theme}>{ui}</ThemeProvider>
+		</HistoryRouter>
+	)
+
+export * from '@testing-library/react'
+export { customRender as render }
